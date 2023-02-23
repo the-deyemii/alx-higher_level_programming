50fc9becac75023f02f6acb34d57977900c053ef
the-deyemii <tellytales0@gmail.com>
2023-02-23 00:48:57 +0000

    ggg

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except (ValueError, TypeError, ZeroDivisionError):
         c = None
     finally:
-        print("Indeed Results: {}".format(c))
+        print("Inside result: {}".format(c))
         return(c)
